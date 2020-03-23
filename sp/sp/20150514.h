6f9123924aa75465f57a1162f2a8f1fa753b36d4
kgw4073 <kgw4073@naver.com>
2020-03-23 22:29:10 +0900

    struct Hash contruction

--- sp/sp/20150514.h
+++ sp/sp/20150514.h
@@ -1,27 +1,35 @@
-
+﻿
 #ifndef _MAIN_HEADER_H
 #define _MAIN_HEADER_H
 #define MAX_COMMAND_SIZE 256
 #define MAX_MEMORY_LINE (1 << 16)
 #define MAX_BYTES_LINE (1 << 4)
+#define MAX_HASH_SIZE 20
 #define bool int
 #define true 1
 #define false 0
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <math.h>
+#define FILE_ERROR(); { fprintf(stderr, "cannot find the file...\n"); }
+#define ALLOCATION_ERROR(); { fprintf(stderr, "Memory allocation error. Please quit and retry"); }
 
 
+
+//struct dumped {
+//	int last_address;
+//};
+typedef struct OpNode {
+	int value;
+	int decimal;
+	char code[20];
+	struct OpNode* next;
+} OpNode;
+
+OpNode* hashTable[MAX_HASH_SIZE];
+OpNode* hashTailPointer[MAX_HASH_SIZE];
 unsigned char vMemory[MAX_MEMORY_LINE * MAX_BYTES_LINE];
 
 extern bool quit_flag;
-struct dumped {
-	int last_address;
-};
+
 extern void init();
+extern void makeHashTable();
+
 #endif
