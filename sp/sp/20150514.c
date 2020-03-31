ad983051a8d71f12f0029da30efefa3cb13c77f1
kgw4073 <kgw4073@naver.com>
2020-03-31 21:46:00 +0900

    Merge BasicCommand into 20150514 (Trie function)

--- sp/sp/20150514.c
+++ sp/sp/20150514.c
@@ -10,8 +10,59 @@
 #include "command.h"
 #include "BasicCommand.h"
 
+#define CTOI(x) ((int)x-(int)'a')
+
 bool quit_flag = false;
 Trie* root = NULL;
+
+Trie* getNewTrieNode() {
+	Trie* node = NULL;
+	node = (Trie*)malloc(sizeof(Trie));
+	if (node) {
+		node->terminal = false;
+		for (int i = 0; i < MAX_ALPHA; i++) {
+			node->child[i] = NULL;
+		}
+	}
+	return node;
+}
+void deleteTrie(struct Trie* root) {
+	if (root) {
+		for (int i = 0; i < MAX_ALPHA; i++) {
+			if (root->child[i]) {
+				deleteTrie(root->child[i]);
+			}
+		}
+		free(root);
+	}
+}
+
+void insertTrie(Trie* root, char* key) {
+	size_t length = strlen(key);
+	Trie* temp = root;
+	for (size_t i = 0; i < length; i++) {
+		int next = CTOI(key[i]);
+		if (!temp->child[next]) {
+			temp->child[next] = getNewTrieNode();
+		}
+		temp = temp->child[next];
+	}
+	temp->terminal = true;
+}
+
+bool searchTrie(Trie* root, char* key) {
+	size_t length = strlen(key);
+	Trie* temp = root;
+
+	for (size_t i = 0; i < length; i++) {
+		int next = CTOI(key[i]);
+		if (!temp->child[next]) {
+			return false;
+		}
+		temp = temp->child[next];
+	}
+	return temp != NULL && temp->terminal;
+}
 
 void init() {
 	char basicInstruction[17][100] = {
@@ -30,6 +81,8 @@ void init() {
 	head_of_command_queue = (command_list*)malloc(sizeof(command_list));
 	tail_of_command_queue = head_of_command_queue;
 }
+
+
 OpNode* getNewHashNode() {
 	OpNode* temp = (OpNode*)malloc(sizeof(OpNode));
 	if (temp == NULL) {
@@ -93,7 +146,7 @@ void makeHashTable() {
 		}
 		insertHashEntry(OpCodeDecimal, value, code);
 	}
-	close(fp);
+	fclose(fp);
 }
 
 
