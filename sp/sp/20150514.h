9ed8d158a1e7ad06baf7a92938b48d9b03a6dfbb
kgw4073 <kgw4073@naver.com>
2020-03-31 21:46:16 +0900

    Merge BasicCommand into 20150514 (Trie function)

--- sp/sp/20150514.h
+++ sp/sp/20150514.h
@@ -10,12 +10,14 @@
 #define false 0
 #define FILE_ERROR(); { fprintf(stderr, "cannot find the file...\n"); }
 #define ALLOCATION_ERROR(); { fprintf(stderr, "Memory allocation error. Please quit and retry"); }
+#define MAX_ALPHA 26
+
+typedef struct Trie {
+	bool terminal;
+	struct Trie* child[MAX_ALPHA];
+} Trie;
 
 
-
-//struct dumped {
-//	int last_address;
-//};
 typedef struct OpNode {
 	int value;
 	int decimal;
@@ -23,8 +25,11 @@ typedef struct OpNode {
 	struct OpNode* next;
 } OpNode;
 
+
+Trie* root;
 OpNode* hashTable[MAX_HASH_SIZE];
 OpNode* hashTailPointer[MAX_HASH_SIZE];
+
 unsigned char vMemory[MAX_MEMORY_LINE * MAX_BYTES_LINE];
 
 extern bool quit_flag;
@@ -32,4 +37,10 @@ extern bool quit_flag;
 extern void init();
 extern void makeHashTable();
 
+
+
+extern Trie* getNewTrieNode();
+extern void deleteTrie(Trie* root);
+extern void insertTrie(Trie* root, char* key);
+extern bool searchTrie(Trie* root, char* key);
 #endif
